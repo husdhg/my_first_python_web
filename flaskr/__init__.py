fe1e4a805f07d32cb1b3ba156e11c3aa0ef2f583
user <964628176@qq.com>
2019-01-02 18:12:23 +0800

    学习备注

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,38 +11,64 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
+    # 创建flask实例，__name__为当前Python模块名称
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    app.config.from_mapping(     # 应用的缺省配置
+        SECRET_KEY='dev', # 是被 Flask 和扩展用于保证数据安全的。在开发过程中， 为了方便可以设置为 'dev' ，但是在发布的时候应当使用一个随机值来 重载它。
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'), # SQLite 数据库文件存放在路径。
     )
-
-    if test_config is None:
+     #判断是否测试
+    if test_config is None: # test_config 也会被传递给工厂，并且会替代实例配置。这样可以实现 测试和开发的配置分离，相互独立。
         # load the instance config, if it exists, when not testing
+        # 使用 config.py 中的值来重载缺省配置，如果 config.py 存在的话。 例如，当正式部署的时候，用于设置一个正式的 SECRET_KEY 。
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-
+    # 确定instance文件存在
     # ensure the instance folder exists
     try:
+        # 确保 app.instance_path 存在， Flask 不会自动 创建实例文件夹，但是必须确保创建这个文件夹，因为 SQLite 数据库文件会被 保存在里面。
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
+    # 导入调用数据库函数
     from . import db
     db.init_app(app)
 
+    # 使用 app.register_blueprint() 导入并注册 蓝图。新的代码放在工厂函数的尾部返回应用之前。
+    # 注册登录蓝图
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # 使用 app.register_blueprint() 在工厂中 导入和注册蓝图。将新代码放在工厂函数的尾部，返回应用之前。
+    # 博客蓝图
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
+    """
+    与验证蓝图不同，博客蓝图没有 url_prefix 。
+    因此 index 视图会用于 / ， create 会用于 /create ，以此类推。
+    博客是 Flaskr 的主要 功能，因此把博客索引作为主索引是合理的。
+    但是，下文的 index 视图的端点会被定义为 blog.index 。一些验证视图 会指定向普通的 index 端点。 
+    我们使用 app.add_url_rule() 关联端点名称 'index' 和 / URL ，这样 url_for('index') 或 url_for('blog.index') 都会有效，
+    会生成同样的 / URL 。
+    在其他应用中，可能会在工厂中给博客蓝图一个 url_prefix 并定义一个独立的 index 视图，类似前文中的 hello 视图。
+    在这种情况下 index 和 blog.index 的端点和 URL 会有所不同。
+    """
 
     return app
+
+
+
+"""
+    Windows 下命令行运行flask：
+    set FLASK_APP=flaskr
+    set FLASK_ENV=development
+    flask run
+    初始化数据库文件：
+    flask init-db
+    Initialized the database.
+    会在instance 中出现 flaskr.sqlite 文件
+"""
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -18,6 +18,7 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
+    # 索引会显示所有帖子，最新的会排在最前面。为了在结果中包含 user 表中的 作者信息，使用了一个 JOIN 。
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
@@ -25,7 +26,9 @@ def index():
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
-
+# 创建视图
+# create 视图与 register 视图原理相同。要么显示表单，要么发送内容 已通过验证且内容已加入数据库，或者显示一个出错信息。
+# 先前写的 login_required 装饰器用在了博客视图中，这样用户必须登录以后 才能访问这些视图，否则会被重定向到登录页面。
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -51,7 +54,9 @@ def create():
 
     return render_template('blog/create.html')
 
-#更新
+# 验证
+# update 和 delete 视图都需要通过 id 来获取一个 post ，并且 检查作者与登录用户是否一致。
+# 为避免重复代码，可以写一个函数来获取 post ， 并在每个视图中调用它。
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -60,15 +65,24 @@ def get_post(id, check_author=True):
         (id,)
     ).fetchone()
 
+    # abort() 会引发一个特殊的异常，返回一个 HTTP 状态码。它有一个可选参数， 用于显示出错信息，若不使用该参数则返回缺省出错信息。
+    # 404 表示“未找到”， 403 代表“禁止访问”。（ 401 表示“未授权”，但是我们重定向到登录 页面来代替返回这个状态码）
     if post is None:
         abort(404, "Post id {0} doesn't exist.".format(id))
-
+    # check_author 参数的作用是函数可以用于在不检查作者的情况下获取一个 post 。
+    # 这主要用于显示一个独立的帖子页面的情况，因为这时用户是谁没有关系， 用户不会修改帖子。
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
 
     return post
 
-
+# 和所有以前的视图不同， update 函数有一个 id 参数。该参数对应路由中 的 <int:id> 。
+# 一个真正的 URL 类似 /1/update 。 Flask 会捕捉到 URL 中的 1 ，确保其为一个 int ，并将其作为 id 参数传递给视图。
+# 如果没有指定 int: 而是仅仅写了 <id> ，那么将会传递一个字符串。
+# 要生成一个指向更新页面的 URL ，需要传递 id 参数给 url_for() ： url_for('blog.update', id=post['id']) 。
+# create 和 update 视图看上去是相似的。
+# 主要的不同之处在于 update 视图使用了一个 post 对象和一个 UPDATE 查询代替了一个 INSERT 查询。
+# 作为一个明智的重构者，可以使用 一个视图和一个模板来同时完成这两项工作。但是作为一个初学者，把它们分别处理 要清晰一些。
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
@@ -96,7 +110,8 @@ def update(id):
 
     return render_template('blog/update.html', post=post)
 
-
+# 删除视图没有自己的模板。删除按钮已包含于 update.html 之中，该按钮指向 /<id>/delete URL 。
+# 既然没有模板，该视图只处理 POST 方法并重定向到 index 视图。
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
